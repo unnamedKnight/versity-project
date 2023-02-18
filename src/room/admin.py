ada5c7aeb66ba68d9c8d00fe623bd4a11cc47625
unnamedKnight <backfrontdev47@gmail.com>
2023-02-18 16:38:07 +0600

    Solve Case sesitivity problem for topics

--- src/room/admin.py
+++ src/room/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Topic, Room, RoomComment
+
 # Register your models here.
+
+
+admin.site.register(Topic)
+admin.site.register(Room)
+admin.site.register(RoomComment)
--- src/room/serializers.py
+++ src/room/serializers.py
@@ -3,20 +3,43 @@ from rest_framework import serializers
 
 from .models import Topic, Room, RoomComment
 
+
 class TopicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Topic
         fields = "__all__"
 
+    def _get_or_create_topic(self, topic):
+        """Handle getting or creating topic as needed."""
+        topic_obj, created = Topic.objects.get_or_create(
+            defaults={"name": topic["name"]}, title__iexact=topic["name"]
+        )
+        return topic_obj.lower()
+
+    def create(self, validated_data):
+        """Create a topic or get an existing topic"""
+        # removing tag from validated data
+        topic = validated_data.pop("name", None)
+        filtered_topic = self._get_or_create_topic(self, topic.lower())
+        topic = Topic.objects.create(name=filtered_topic)
+        return topic
+
+
+    def update(self, instance, validated_data):
+        """Update a topic"""
+        topic = validated_data.pop("name", None)
+        instance.name = self._get_or_create_topic(topic.lower())
+        instance.save()
+        return instance
+
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = "__all__"
+        fields = ("topic", "name", "description")
+
 
 class RoomCommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = RoomComment
-        fields = "__all__"
-
-
+        fields = ("body",)
