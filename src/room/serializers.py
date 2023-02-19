3d89057a67704c91d0621508bf67aeb5772e30ba
unnamedKnight <backfrontdev47@gmail.com>
2023-02-19 14:06:35 +0600

    Add Create Room Functionality

--- src/room/serializers.py
+++ src/room/serializers.py
@@ -9,34 +9,64 @@ class TopicSerializer(serializers.ModelSerializer):
         model = Topic
         fields = "__all__"
 
-    def _get_or_create_topic(self, topic):
-        """Handle getting or creating topic as needed."""
-        topic_obj, created = Topic.objects.get_or_create(
-            defaults={"name": topic["name"]}, title__iexact=topic["name"]
-        )
-        return topic_obj.lower()
+    # def _get_or_create_topic(self, topic):
+    #     """Handle getting or creating topic as needed."""
+    #     topic_obj, created = Topic.objects.get_or_create(
+    #         defaults={"name": topic["name"]}, title__iexact=topic["name"]
+    #     )
+    #     return topic_obj
 
-    def create(self, validated_data):
-        """Create a topic or get an existing topic"""
-        # removing tag from validated data
-        topic = validated_data.pop("name", None)
-        filtered_topic = self._get_or_create_topic(self, topic.lower())
-        topic = Topic.objects.create(name=filtered_topic)
-        return topic
+    # def create(self, validated_data):
+    #     """Create a topic or get an existing topic"""
+    #     # removing tag from validated data
+    #     topic = validated_data.pop("name", None)
+    #     topic_obj = self._get_or_create_topic(self, topic.lower())
+    #     return topic_obj
 
-
-    def update(self, instance, validated_data):
-        """Update a topic"""
-        topic = validated_data.pop("name", None)
-        instance.name = self._get_or_create_topic(topic.lower())
-        instance.save()
-        return instance
+    # def update(self, instance, validated_data):
+    #     """Update a topic"""
+    #     topic = validated_data.pop("name", None)
+    #     instance.name = self._get_or_create_topic(topic.lower())
+    #     instance.save()
+    #     return instance
 
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ("topic", "name", "description")
+        # extra_kwargs = {"topic": {"read_only": True}}
+
+    # def _get_or_create_topic(self, topic):
+    #     """Handle getting or creating topic as needed."""
+    #     topic_obj, created = Topic.objects.get_or_create(
+    #         defaults={"name": topic}, name__iexact=topic
+    #     )
+    #     return topic_obj.id
+
+    # def validate_topic(self, value):
+    #     if not value:
+    #         raise serializers.ValidationError('Please provide a Topic name.')
+    #     return self._get_or_create_topic(value)
+
+    # def create(self, validated_data):
+    #     """Create a Room"""
+
+    #     topic = validated_data.pop('topic')
+    #     topic_instance, created = Topic.objects.get_or_create(name=topic)
+    #     room = Room.objects.create(**validated_data, topic=topic_instance)
+    #     room.save()
+    #     return room
+
+    # def update(self, instance, validated_data):
+    #     """Update Room."""
+    #     topic = validated_data.pop("topic", None)
+    #     clean_topic, _ = Topic.objects.get_or_create(name=topic)
+    #     instance.topic = clean_topic
+    #     for attr, value in validated_data.items():
+    #         setattr(instance, attr, value)
+    #     instance.save()
+    #     return instance
 
 
 class RoomCommentSerializer(serializers.ModelSerializer):
--- src/room/views.py
+++ src/room/views.py
@@ -1,10 +1,104 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import get_user_model
 
+from .models import Topic, Room, RoomComment
+from .serializers import RoomSerializer, RoomCommentSerializer
 
 
 User = get_user_model()
 
 # Create your views here.
+
+
+class AllRoomsView(APIView):
+    """
+    List all rooms, or create a new room.
+    """
+
+    def get(self, request, format=None):
+        rooms = Room.objects.all()
+        serializer = RoomSerializer(rooms, many=True)
+        return Response({"status": status.HTTP_200_OK, "data": serializer.data})
+
+    def post(self, request, format=None):
+        serializer = RoomSerializer(data=request.data)
+        # getting the value of topic field from request.data
+        topic = request.data.get("topic")
+        # --------------------- working part --------------------- #
+        # we are getting or creating a new topic_obj
+        # then assigning serializer_initial data with that value
+        topic_obj, _ = Topic.objects.get_or_create(
+            defaults={"name": topic}, name__iexact=topic
+        )
+        serializer.initial_data._mutable = True
+        serializer.initial_data["topic"] = topic_obj.id
+        serializer.initial_data._mutable = False
+
+        # -------------------------- end ------------------------- #
+        if serializer.is_valid():
+            serializer.save(host=request.user)
+            return Response(
+                {
+                    "status": status.HTTP_201_CREATED,
+                    "message": "New Room created successfully",
+                    "data": serializer.data,
+                }
+            )
+        return Response(
+            {"status": status.HTTP_400_BAD_REQUEST, "error": serializer.errors}
+        )
+
+
+class RoomDetailView(APIView):
+    """
+    Returns the details of a Room instance
+    """
+
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist as e:
+            raise Http404 from e
+
+    def get(self, request, pk, format=None):
+        room = self.get_object(pk)
+        serializer = RoomSerializer(room)
+        return Response({"status": status.HTTP_200_OK, "data": serializer.data})
+
+
+class UpdateRoomView(APIView):
+    """
+    Update a room instance
+    """
+
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist as e:
+            raise Http404 from e
+
+    def put(self, request, pk, format=None):
+        room = self.get_object(pk)
+        if room.host != request.user:
+            return Response(
+                {
+                    "status": status.HTTP_401_UNAUTHORIZED,
+                    "message": "You do not have permission to perform this action",
+                }
+            )
+        serializer = RoomSerializer(room, data=request.data)
+        if serializer.is_valid():
+            serializer.save(host=request.user)
+            return Response(
+                {
+                    "status": status.HTTP_200_OK,
+                    "message": "Room updated successfully",
+                    "data": serializer.data,
+                }
+            )
+        return Response(
+            {"status": status.HTTP_400_BAD_REQUEST, "error": serializer.errors}
+        )
